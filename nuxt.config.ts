09d02cee05dc65395164b9b63637b5001df97216
manhdinh <manhdinh141@gmail.com>
2022-11-11 18:10:32 +0700

    TODO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,40 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'ElementPlus + Nuxt3',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'ElementPlus + Nuxt3',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  modules: [
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@element-plus/nuxt',
+  ],
 
+  css: ['~/assets/index.scss', '~/assets/fonts.scss'],
+  experimental: {
+    reactivityTransform: true,
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/element/index.scss" as element;',
+        },
+      },
+    },
+  },
+  elementPlus: {
+    icon: 'ElIcon',
+    importStyle: 'scss',
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { FormInstance } from 'element-plus'
+
+useHead({ title: 'Home' })
+
+const form = reactive({})
+const formRef = ref<FormInstance>()
+</script>
+
+<template lang="pug">
+el-card(shadow="never" header="Organization")
+  el-form(ref="formRef" :model="form")
+    el-row(:gutter="15")
+      el-col(:sm="24")
+        el-form-item(prop="brand_id" label="Brand")
+          el-input(v-model="form.bran_id" id="123")
+        el-form-item
+          el-button(type="primary") This is Button
+</template>
+
+<style lang="scss" scoped>
+.root {
+  background: antiquewhite;
+  color: black;
+}
+</style>
